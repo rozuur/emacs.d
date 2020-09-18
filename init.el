ffc4906e319ffc9c61d9c7514397e001c1716d96
Naveen Vardhi <rozuur@gmail.com>
2020-09-18 16:18:33 +0530

    Use leuven theme

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el -*- lexical-binding: t; -*-
+;;; init.el -*- coding: utf-8; lexical-binding: t; -*-
 
 (let* ((minver "27.1"))
   (when (version< emacs-version minver)
@@ -6,6 +6,9 @@
 
 (setq emacs-load-start-time (current-time))
 (setq user-emacs-directory (file-name-directory load-file-name))
+
+;; Load emacs libraries from ~/.emacs.d/lisp directory
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; A big contributor to startup times is garbage collection. We up the gc
 ;; threshold to temporarily prevent it from running, then reset it later by
@@ -34,8 +37,16 @@
   ;; (benchmark-init/activate)
   ;; ;; `benchmark-init/show-durations-tree' to show benchmark result
   ;; ;; }}
+
+  ;; Bootstrap straight.el
+  (require 'straight (concat user-emacs-directory "straight.el"))
+
+  ;; Load rest of the libraries
+  (require 'init-looks)
+  
   )
 
+;; For now instead of gchm, reset manually
 (setq best-gc-cons-threshold (* 64 1024 1024))
 (setq gc-cons-threshold best-gc-cons-threshold)
 
@@ -43,7 +54,6 @@
   (message "Emacs startup time: %d seconds."
            (time-to-seconds (time-since emacs-load-start-time))))
 
-;;; Local Variables:
 ;;; no-byte-compile: t
 ;;; End:
 (put 'erase-buffer 'disabled nil)
