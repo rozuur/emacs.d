c01fff39e016ef36aeb902830870639efa2030c9
Naveen Vardhi <rozuur@gmail.com>
2020-09-18 23:52:08 +0530

    Press c in ediff to use both changes

--- lisp/init-ediff.el
+++ lisp/init-ediff.el
@@ -4,13 +4,16 @@
 (defun ediff-startup-hook-setup ()
   ;; hide control panel if it's current buffer
   (when (string-match-p "\*Ediff Control Panel.*\*" (buffer-name))
-    ;; load color theme for merge
-    (load-theme 'tango t)
     ;; show only clashed area
     (ediff-toggle-show-clashes-only)
     ;; move to the first difference
     (ediff-next-difference)))
 
+(add-hook 'ediff-startup-hook 'ediff-startup-hook-setup)
+
+
+;; https://stackoverflow.com/a/29757750/281402
+;; Create a merge with a copy of both differences
 (defun ediff-copy-both-to-C ()
   (interactive)
   (ediff-copy-diff ediff-current-difference nil 'C nil
@@ -18,9 +21,9 @@
                     (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
 
-(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
-(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+;; And press 'c' to activate the merge
+(defun add-c-to-ediff-mode-map () (define-key ediff-mode-map "c" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-c-to-ediff-mode-map)
 
-(add-hook 'ediff-startup-hook 'ediff-startup-hook-setup)
 
 (provide 'init-ediff)
